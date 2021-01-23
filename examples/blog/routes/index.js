0c1215cad433bb7469666566240f9ede207eac33
wadahkode <mvp.dedefilaras@gmail.com>
2021-01-23 10:36:10 +0700

    add password-hash

--- examples/blog/routes/index.js
+++ examples/blog/routes/index.js
@@ -1,12 +1,16 @@
 const wadahkode = require('../../../'),
   Router = wadahkode().Router,
   client = wadahkode().Client,
-  session = require('../../../lib/v2/session')();
+  session = require('../../../lib/v2/session')(),
+  passwordHash = require('@wadahkode/password-hash');
 
 session.start();
 client.initialize({
   path: wadahkode().dirname('examples/blog') + '/.env',
 });
+
+let salt = passwordHash.generateSalt(10),
+  hashedPassword = "";
 
 Router.get('/', (req, res) => {
   res.render('index', {
@@ -33,21 +37,36 @@ Router.get('/admin', (req, res) => {
     : res.render('admin/index', {title: 'Blog'});
 });
 
+// Register GET
+Router.get('/admin/register', (req, res) => {
+  return session.has('superuser')
+    ? res.redirect('/admin/dashboard')
+    : res.render('admin/register', {title: 'Blog'});
+});
+
+
 // Admin Sign-in
 Router.post('/admin/signin', (req, res) => {
   const {db} = client.connect();
-  db.connect();
   
-  db.query('select * from users where name=$1 and password=$2;', [req.body.username, req.body.password], (err, snapshot) => {
+  db.connect();
+  db.query('SELECT * FROM users WHERE username=$1', [req.body.username], (err, snapshot) => {
     if (!err) {
       if (snapshot.rows.length < 1) {
-        res.end('login gagal!');
+        res.end('Nama pengguna salah atau akun belum terdaftar!');
       } else {
-        res.end('login berhasil!');
+        for (let user of snapshot.rows) {
+          if (passwordHash.verify(req.body.password, JSON.parse(user.password))) {
+            res.end('login berhasil!');
+          } else {
+            res.end('kata sandi salah!');
+          }
+        }
       }
     } else {
-      db.end('Koneksi error!');
+      res.end('Akun belum terdaftar!');
     }
+    db.end();
   });
   // if (session.has('superuser')) {
   //   res.redirect('/admin/dashboard');
@@ -59,6 +78,48 @@ Router.post('/admin/signin', (req, res) => {
   //     res.end('login gagal!');
   //   }
   // }
+});
+// Admin Sign-up
+Router.post('/admin/signup', (req, res) => {
+  const {db} = client.connect();
+    date = new Date();
+  
+  if (req.body.agreement != undefined) {
+    if (req.body.password != req.body.passwordVerify) {
+      res.end('Password tidak dapat dicocokan!');
+    } else {
+      hashedPassword = JSON.stringify(passwordHash.hash(req.body.password, salt));
+    }
+    const users = {
+      get: function(query, id, callback) {
+        db.connect();
+        db.query(query, id, (err, result) => {
+          if (!err) {
+            return callback(result.rows.length >= 1 ? true : false);
+          }
+        });
+      }
+    };
+    
+    users.get('select * from users where email=$1', [req.body.email], (status) => {
+      if (status) {
+        res.end('akun sudah terdaftar!');
+      } else {
+        const query = 'INSERT INTO users(username, email, password, password_verify, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+        const values = [req.body.username, req.body.email, hashedPassword, hashedPassword, date, date];
+        db.query(query, values, (error, result) => {
+          if (!error) {
+            res.end('Akun berhasil dibuat, silahkan login untuk melanjutkan!');
+          } else {
+            console.log(error.stack);
+            res.end('Gagal membuat akun!');
+          }
+        });
+      }
+    });
+  } else {
+    res.end('Tolong terima syarat dan ketentuan yang berlaku!');
+  }
 });
 
 // Dashboard
