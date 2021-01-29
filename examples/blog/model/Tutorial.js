39c58491568e82fd6fa4423e3e3c4eb0ef5a1a9a
wadahkode <mvp.dedefilaras@gmail.com>
2021-01-29 11:38:11 +0700

    clean code

--- examples/blog/model/Tutorial.js
+++ examples/blog/model/Tutorial.js
@@ -1,8 +1,8 @@
 /**
- * User Model
+ * Tutorial Model
  * 
  * @author wadahkode <mvp.dedefilaras@gmail.com>
- * @since version 1.1.6
+ * @since version 1.1.8
  */
 let db;
 
@@ -46,31 +46,17 @@ tutorialModel.push = (table, params, callback) => {
 
 // Update data
 tutorialModel.update = (table, params, id, callback) => {
-  let tableName = "",
-    tableKey = "",
-    tableValues = "",
-    values = "",
+  let values = "",
     tableCountable = 1;
 
   Object.keys(params).map(item => {
     values += `${item}='${params[item]}',`;
-    // tableName += item + '= ';
-    // tableValues += params[item] + '%%%';
 
-    // tableKey += '$' + tableCountable + ', ';
     tableCountable++;
-  })
-  // tableName = tableName.slice(0, -2);
-  // tableValues = tableValues.slice(0, -3);
-  // tableKey = tableKey.slice(0, -2);
+  });
 
   const query = `UPDATE ${table} SET ${values.slice(0, -1)} WHERE id=${id} RETURNING *`;
   return db.update(query).then(error => callback(error));
-
-  // const query = `UPDATE ${table}(${tableName}) VALUES(${tableKey}) RETURNING *`;
-  // const values = tableValues.split('%%%');
-
-  // return db.push(query, values).then(error => callback(error));
 };
 
 // Delete data
