80d876e7a4ef933c0065b3ba40e9f48d54822f4f
wadahkode <mvp.dedefilaras@gmail.com>
2021-02-07 19:53:30 +0700

    add @wadahkode/memories and other

--- src/view/index.ts
+++ src/view/index.ts
@@ -1,17 +1,20 @@
 /**
  * View
  */
-const View = function(view: any) {
-  if (typeof view.engine == 'object') {
-    const {renderFile} = view.engine;
-      
+const View = function (view: any) {
+  if (typeof view.engine == "object") {
+    const { renderFile } = view.engine;
     return {
-      render: function(callback: any) {
-        return renderFile(view.filename, view.data, view.options, (err: any, str: any) => callback(err, str));
-      }
+      render: function (callback: any) {
+        return renderFile(
+          view.filename,
+          view.data,
+          view.options,
+          (err: any, str: any) => callback(err, str)
+        );
+      },
     };
-    
   }
-}
+};
 
 module.exports = View;
